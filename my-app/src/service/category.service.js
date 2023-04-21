ee95f3c40a0fba67262a415373d4017127057d25
hoang0801 <lehoang08012001@gmail.com>
2023-04-22 00:32:24 +0700

    Update category.service.js

---
+++ my-app/src/service/category.service.js
@@ -0,0 +1,50 @@
+
+
+// export const deleteCategoryAPI = async (id) =>{
+//     var config ={
+//         method: 'DELETE',
+//         URL:`/admin/category/delete?id=$(id)`
+//     }
+
+//     return handleResponse(config);
+// };
+
+//  export const findCategoriesAPI = async (search) =>{
+//     var config ={
+//         method: 'post',
+//         url:'/category/search',
+//         data: search
+//     }
+//     return handleResponse(config);
+//  };
+
+//  export const getCategoryByIdAPI = async (id)=>{
+//     var config ={
+//         method: 'get',
+//         url:' / category/${id}'
+//     }
+//     return handleResponse(config);
+//  };
+
+//  export const addCategoryAPI = async (category) =>{
+//     var config ={
+//         method:'post',
+//         url:'/ admin/category/add',
+//         data: category
+//     }
+//     return handleResponse(config)
+//  };
+
+//  export const updateCategoryAPI = async (category){
+//     var config ={
+//         method:'put',
+//         url:'/admin/categor/update',
+//         data: catefory
+//     };
+//      return handleResponse(config);
+//  };
+
+//  const handleResponse = async (config);
+//  try{
+//     let response = await axiosInstance (config)
+//  };
