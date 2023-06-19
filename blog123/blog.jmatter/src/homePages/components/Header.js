db034898baa581f7d5ed761ee9dd90ed40486b38
hoang0801 <115029047+hoang0801@users.noreply.github.com>
2023-06-19 23:29:10 +0700

    up

--- blog123/blog.jmatter/src/homePages/components/Header.js
+++ blog123/blog.jmatter/src/homePages/components/Header.js
@@ -1,18 +1,98 @@
-import { Icon } from '@iconify/react';
+import { alpha } from '@material-ui/core';
 import AdjustOutlinedIcon from '@mui/icons-material/AdjustOutlined';
-import { Box, Button, ListItem, ListItemButton, ListItemText, Stack } from "@mui/material";
+import SearchIcon from '@mui/icons-material/Search';
+import { Box, Button, Container, InputBase, ListItem, ListItemButton, ListItemText, Stack } from "@mui/material";
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { Link, useNavigate } from 'react-router-dom';
-
+import styled from 'styled-components';
+import { searchPost, setPostSearch } from '../../redux/postSlice';
 
 export default function TopBar1() {
-  let navigate = useNavigate();
-  let handleChangeLogin = () => {
-    return (
-      navigate("/login")
-    );
-  };
-  return (
+ 
 
+  const { posts, recordsFiltered, search, error } = useSelector((state) => state.post);
+  const Navigate = useNavigate()
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    console.log(posts.map);
+
+    const timeout = setTimeout(() => {
+      find();
+    }, 500);
+    return () => clearTimeout(timeout);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [search]); // khi redux seearch thay doi, thi se dc goi lai find
+
+  const find = async () => {
+    dispatch(searchPost());
+  };
+
+  const handleChangeLogin =() =>{
+    return(
+      Navigate("/login")
+    )
+  }
+
+  const handleChange = (e) => {
+    let newSearch = {
+      ...search,
+      start: 0,// reset lai trang dau
+      [e.target.name]: e.target.value
+    };
+
+    //update thay doi redux search
+    dispatch(setPostSearch(newSearch));
+  };
+  
+
+  const Search = styled('div')(({ theme }) => ({
+    position: 'relative',
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: alpha(theme.palette.common.white, 0.15),
+    '&:hover': {
+      backgroundColor: alpha(theme.palette.common.white, 0.25),
+    },
+    marginLeft: 0,
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      marginLeft: theme.spacing(1),
+      width: 'auto',
+    },
+  }));
+  
+  const SearchIconWrapper = styled('div')(({ theme }) => ({
+    padding: theme.spacing(0, 2),
+    height: '100%',
+    position: 'absolute',
+    pointerEvents: 'none',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  }));
+  
+  const StyledInputBase = styled(InputBase)(({ theme }) => ({
+    color: 'inherit',
+    '& .MuiInputBase-input': {
+      padding: theme.spacing(1, 1, 1, 0),
+      // vertical padding + font size from searchIcon
+      paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+      transition: theme.transitions.create('width'),
+      width: '100%',
+      [theme.breakpoints.up('sm')]: {
+        width: '12ch',
+        '&:focus': {
+          width: '20ch',
+        },
+      },
+    },
+  }));
+
+ 
+ 
+  return (
+<Container>
     < Box >
       <Stack direction="row" spacing={-1}>
         <ListItem disablePadding>
@@ -67,11 +147,16 @@ export default function TopBar1() {
         </Stack>
 
         <ListItem disablePadding>
-          <Box marginLeft={5}>
-            <Button>
-              <Icon icon="fluent-mdl2:zoom" />
-            </Button>
-          </Box>
+        
+        <Search>
+            <SearchIconWrapper>
+              <SearchIcon />
+            </SearchIconWrapper>
+            <StyledInputBase
+              placeholder="Search…"
+              inputProps={{ 'aria-label': 'search' }}
+            />
+          </Search>
         </ListItem>
 
         <ListItem disablePadding>
@@ -83,6 +168,7 @@ export default function TopBar1() {
       </Stack>
 
     </Box >
+    </Container>
   );
 }
 
